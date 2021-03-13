05f3334bf8ab245c4e70a18b363a7aebe2f86faf
pigooosuke <hifirole@gmail.com>
2021-03-13 14:32:40 +0900

    add sol

--- lessons/lesson16.py
+++ lessons/lesson16.py
@@ -3,23 +3,36 @@ from typing import List
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        ans = None
-        max_diff = None
-        for i, val in enumerate(nums[1:-1]):
-            # print(nums[i: i + 3])
-            vals = sum(nums[i: i + 3])
-            diff = abs(target - vals)
-            if max_diff is None:
-                max_diff = diff
-                ans = vals
+        ans = float("inf")
+        nums.sort()
+        # print(nums)
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i-1]:
+                ans = self.twosum(i, nums, target, ans)
+        return ans
+
+    def twosum(self, i: int, nums: List[int], target: int, ans: float) -> int:
+        lo, hi = i+1, len(nums) - 1
+        while (lo < hi):
+            sum_v = nums[i] + nums[lo] + nums[hi]
+            # print(f"{i=}, {lo=}, {hi=}, {ans=} {sum_v=}")
+            if abs(target - ans) > abs(target - sum_v):
+                ans = sum_v
+            if target > sum_v:
+                lo += 1
+                # memory less
+                while (lo < hi and lo > 2 and nums[lo - 1] == nums[lo] == nums[lo+1]):
+                    lo += 1
+            elif target < sum_v:
+                hi -= 1
+                # memory less
+                while (lo < hi and hi < len(nums) - 2 and nums[hi-1] == nums[hi] == nums[hi+1]):
+                    hi -= 1
             else:
-                if max_diff > diff:
-                    max_diff = diff
-                    ans = vals
-            # print(f"{diff=}, {ans=}")
+                return ans
         return ans
 
 
 solution = Solution()
-ans = solution.threeSumClosest(nums=[-1, 2, 1, -4], target=1)
-print(f"{ans}")
+ans = solution.threeSumClosest(nums=[1, 1, -1, -1, 3], target=1)
+print(f"{ans=}")
